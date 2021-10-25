72933756dc0061753df8242fc0cebb31d2615c41
Andrianarivo David <87760993+narivo@users.noreply.github.com>
2021-10-25 17:33:35 +0300

    project 10

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCellCollectionViewCell.swift
+//  Project10
+//
+//  Created by MadiApps on 25/10/2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by MadiApps on 25/10/2021.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("internal error: cell Person does not have class PersonCell")
+        }
+        return cell
+    }
+        
+    /*func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 140, height: 180)
+    }*/
+
+}
