17981ef0481a9bdbd74328f8c3eeccf6a006833f
andrianarivo <andrianarivo.david@gmail.com>
2022-01-26 16:16:48 +0300

    project 25 part one working multipeer connectivity

---
+++ Project25/Project25/SelfieMultipeerSession.swift
@@ -0,0 +1,106 @@
+//
+//  SelfieMultipeerSession.swift
+//  Project25
+//
+//  Created by MadiApps on 26/01/2022.
+//
+
+import MultipeerConnectivity
+import os
+
+class SelfieMultipeerSession: NSObject, ObservableObject {
+    
+    private let serviceType = "project25"
+    private let myPeerId = MCPeerID(displayName: UIDevice.current.name)
+    private let serviceAdvertiser: MCNearbyServiceAdvertiser
+    private let serviceBrowser: MCNearbyServiceBrowser
+    private let session: MCSession
+    private let log = Logger()
+    
+    var connectedPeers: [MCPeerID] = []
+    var dataReceiver: MultiPeerDataReceiver?
+    
+    override init() {
+        session = MCSession(peer: myPeerId, securityIdentity: nil, encryptionPreference: .none)
+        serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: serviceType)
+        serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: serviceType)
+
+        super.init()
+
+        session.delegate = self
+        serviceAdvertiser.delegate = self
+        serviceBrowser.delegate = self
+
+        serviceAdvertiser.startAdvertisingPeer()
+        serviceBrowser.startBrowsingForPeers()
+    }
+
+    deinit {
+        serviceAdvertiser.stopAdvertisingPeer()
+        serviceBrowser.stopBrowsingForPeers()
+    }
+    
+    func send(_ data: Data, toPeers connectedPeers: [MCPeerID], with mode: MCSessionSendDataMode) {
+        do {
+            try session.send(data, toPeers: connectedPeers, with: mode)
+        } catch {
+            log.error("Sending data error: \(String(describing: error))")
+        }
+    }
+}
+
+extension SelfieMultipeerSession: MCNearbyServiceAdvertiserDelegate {
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+        log.error("ServiceAdvertiser didNotStartAdvertisingPeer: \(String(describing: error))")
+    }
+
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        log.info("didReceiveInvitationFromPeer \(peerID)")
+        invitationHandler(true, session)
+    }
+}
+
+extension SelfieMultipeerSession: MCNearbyServiceBrowserDelegate {
+    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+        log.error("ServiceBrowser didNotStartBrowsingForPeers: \(String(describing: error))")
+    }
+
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String: String]?) {
+        log.info("ServiceBrowser found peer: \(peerID)")
+        browser.invitePeer(peerID, to: session, withContext: nil, timeout: 10)
+    }
+
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        log.info("ServiceBrowser lost peer: \(peerID)")
+    }
+}
+
+extension SelfieMultipeerSession: MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        log.info("peer \(peerID) didChangeState: \(state.rawValue)")
+        DispatchQueue.main.async { [weak self] in
+            self?.connectedPeers = session.connectedPeers
+        }
+    }
+
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        log.info("didReceive bytes \(data.count) bytes")
+        dataReceiver?.session(session, didReceive: data, fromPeer: peerID)
+    }
+
+    public func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        log.error("Receiving streams is not supported")
+    }
+
+    public func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        log.error("Receiving resources is not supported")
+    }
+
+    public func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        log.error("Receiving resources is not supported")
+    }
+}
+
+protocol MultiPeerDataReceiver {
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID)
+}
---
+++ Project25/Project25/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Project25
+//
+//  Created by MadiApps on 07/01/2022.
+//
+
+import MultipeerConnectivity
+import UIKit
+
+class ViewController: UICollectionViewController,
+                      UICollectionViewDelegateFlowLayout,
+                      UINavigationControllerDelegate,
+                      UIImagePickerControllerDelegate,
+                      MultiPeerDataReceiver {
+
+    var selfieSession = SelfieMultipeerSession()
+    var images = [UIImage]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        
+        selfieSession.dataReceiver = self
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+            
+        }
+        
+        return cell
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        
+        images.insert(image, at: 0)
+        collectionView.reloadData()
+        
+        if let imageData = image.pngData() {
+            selfieSession.send(imageData, toPeers: selfieSession.connectedPeers, with: .reliable)
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        DispatchQueue.main.async { [weak self] in
+            if let image = UIImage(data: data) {
+                self?.images.insert(image, at: 0)
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+}
+
